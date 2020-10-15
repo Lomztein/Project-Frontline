243ecdc886bbde8c34c5bf616b830952f1e09938
Lomzie <lomztein2@gmail.com>
2020-10-15 02:08:48 +0200

    Factory experimentation

---
+++ Assets/Source/UnitFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFactory : MonoBehaviour, IFactionComponent
+{
+    public GameObject UnitPrefab;
+    public float SpawnDelay;
+    public float SpawnRange;
+
+    private Faction _faction;
+
+    public void Start()
+    {
+        InvokeRepeating("Spawn", SpawnDelay, SpawnDelay);
+    }
+
+    private void Spawn ()
+    {
+        Vector3 pos = GetLocalRandomSpawnPosition() + transform.position;
+        _faction.Instantiate(UnitPrefab, pos, transform.rotation);
+    }
+
+    private Vector3 GetLocalRandomSpawnPosition ()
+    {
+        Vector3 unitSphere = Random.insideUnitSphere * SpawnRange;
+        return new Vector3(unitSphere.x, 0f, unitSphere.z);
+    }
+
+    public void SetFaction(Faction faction)
+    {
+        _faction = faction;
+    }
+}
