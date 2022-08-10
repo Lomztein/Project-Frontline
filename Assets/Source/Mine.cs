8aec05aede693e90f6374175aa961845d48cd824
Marcus Lomstein Jensen <lomztein4@gmail.com>
2022-08-10 12:01:50 +0200

    Implemented mine

---
+++ Assets/Source/Mine.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour, ITeamComponent
+{
+    public LayerMask TargetLayer;
+
+    public float TriggerRange;
+    public float JumpVelocity;
+    public float ExplodeDelay;
+
+    public float ExplosionRange;
+    public float ExplosionDamage;
+    public DamageMatrix.Damage ExplosionDamageType;
+
+    public Effect TriggerEffect;
+    public Effect ExplodeEffect;
+
+    public Vector3 Gravity = new Vector3(0f, -9.81f, 0f);
+    private bool _flying;
+    private Vector3 _velocity;
+
+    public void SetTeam(TeamInfo team)
+    {
+        TargetLayer = team.GetOtherLayerMasks();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_flying && Physics.CheckSphere(transform.position, ExplosionRange, TargetLayer))
+        {
+            StartCoroutine(JumpAndExplode());
+        }
+
+        if (_flying)
+        {
+            _velocity += Gravity * Time.fixedDeltaTime;
+            transform.position += _velocity * Time.fixedDeltaTime;
+        }
+    }
+
+    private IEnumerator JumpAndExplode ()
+    {
+        TriggerEffect.Play();
+
+        _velocity += Vector3.up * JumpVelocity;
+        _flying = true;
+
+        yield return new WaitForSeconds(ExplodeDelay);
+        Explode();
+    }
+
+    private void Explode ()
+    {
+        ExplodeEffect.Play();
+        var hits = Physics.OverlapSphere(transform.position, ExplosionRange, TargetLayer);
+        foreach (var hit in hits)
+        {
+            var health = hit.GetComponentInParent<Health>();
+            health.TakeDamage(new DamageInfo(ExplosionDamage, ExplosionDamageType, hit.transform.position, (hit.transform.position - transform.position).normalized));
+        }
+    }
+}
